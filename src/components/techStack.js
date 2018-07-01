86c1ded5019541f68b7b048d4ac4d30ffb267be3
Patrick Huang <patrickhuang94@gmail.com>
2018-07-01 18:34:39 -0500

    Use cards for tech stack

--- src/components/techStack.js
+++ src/components/techStack.js
@@ -1,125 +1,126 @@
 import React from 'react'
 import Radium from 'radium'
 
+import Paper from 'material-ui/Paper'
+
 import {
-	sectionTitleContainer,
-	sectionWidthContainer
+	sectionWidthContainer,
+	sectionTitleContainer
 } from './styles/sharedStyles'
 
 import {
-	centerAlignTextStyle,
-	keywordStyle
-} from './styles/textStyles'
+	cardContainer,
+	paperStyle
+} from './styles/cardStyles'
 
 import FrontendIcon from 'material-ui/svg-icons/editor/mode-edit'
 import BackendIcon from 'material-ui/svg-icons/action/code'
 
-const stackContainer = {
-	display: 'flex',
-	justifyContent: 'center',
-	padding: '30px 0px',
-	width: '100%',
-	background: 'white'
-}
-
-const devStackContainer = {
-	display: 'flex',
-	flexDirection: 'column',
-	margin: '15px 0px 0px 0px',
-	width: '100%'
-}
-
-const codeIconContainer = {
-	display: 'flex',
-	justifyContent: 'center',
-	alignItems: 'center',
-	margin: '0px 15px 0px 0px',
-	'@media screen and (max-width: 649px)': {
-		margin: '0px 0px 15px 0px'
+const styles = {
+	container: {
+		display: 'flex',
+		justifyContent: 'center',
+		paddingTop: 30,
+		width: '100%'
+	},
+	cardStyle: {
+		padding: 25,
+		marginLeft: 25,
+		marginRight: 25,
+		marginBottom: 35,
+		width: '700px'
+	},
+	sectionTitle: {
+		width: '100%',
+		marginBottom: 20,
+		marginLeft: 40
+	},
+	title: {
+		display: 'flex',
+		marginBottom: 20,
+		fontSize: 24,
+	},
+	subtitleContainer: {
+		lineHeight: '24px'
+	},
+	headingStyle: {
+		display: 'flex',
+		justifyContent: 'center',
+		fontSize: 26
+	},
+	iconContainer: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginRight: 15,
+		'@media screen and (max-width: 649px)': {
+			margin: '0px 0px 15px 0px'
+		}
+	},
+	icon: {
+		height: '28px',
+		width: '28px',
+	},
+	subheaderStyle: {
+		lineHeight: '28px'
+	},
+	keyword: {
+		fontWeight: '500',
+		color: '#1FBDC2'
 	}
 }
 
-const codeIconStyle = {
-	height: '35px',
-	width: '35px',
-}
-
-const customSectionTitleContainer = {
-	...sectionTitleContainer,
-	marginBottom: '20px'
-}
-
-const frontendContainer = {
-	width: '100%',
-	marginBottom: '15px'
-}
-
-const backendContainer = {
-	width: '100%'
-}
-
-const subheaderStyle = {
-	lineHeight: '28px'
-}
-
 class TechStack extends React.Component {
-	render() {
-		return (
-			<div style={stackContainer}>
-				<div style={sectionWidthContainer}>
-					<div style={devStackContainer}>
-						<div style={frontendContainer}>
-							<div style={customSectionTitleContainer}>
-								<span style={codeIconContainer}><FrontendIcon style={codeIconStyle} /></span>
-								<h1 style={centerAlignTextStyle}>Front-end Development</h1>
-							</div>
-							<h3 style={subheaderStyle}>I specialize in <span style={keywordStyle}>React</span> applications. My expertise in front-end development includes:</h3>
-							<ul>
-								<li>
-									Creating responsive client-side web applications with
-									Flexbox and Radium
-								</li>
-								<li>
-									Maintaining state management with Redux
-								</li>
-								<li>
-									Using higher-order components to increase
-									code reusability
-								</li>
-								<li>
-									Extensive usage of ES6 destructuring, arrow functions and promises
-								</li>
-								<li>
-									Generating responsive email templates with Foundation for Email
-								</li>
-							</ul>
-						</div>
 
-						<div style={backendContainer}>
-							<div style={customSectionTitleContainer}>
-								<span style={codeIconContainer}><BackendIcon style={codeIconStyle} /></span>
-								<h1 style={centerAlignTextStyle}>Back-end Development</h1>
-							</div>
-							<h3 style={subheaderStyle}>I work with <span style={keywordStyle}>Node.js</span>. My proficiency with back-end development includes:</h3>
-							<ul>
-								<li>
-									Developing backend services using RESTful architecture
-								</li>
-								<li>
-									Integration of third party API services (Facebook, Twitter, and Instagram)
-								</li>
-								<li>
-									Basic knowledge of cloud storage services (Amazon S3) and databases (MySQL)
-								</li>
-								<li>
-									Performing integration testing with Mocha, Sinon and Chai
-								</li>
-								<li>
-									Deploying application to server with Git and Jenkins
-								</li>
-							</ul>
-						</div>
+	renderBackendContainer () {
+
+		return (
+			<div style={cardContainer}>
+				<Paper style={paperStyle} zDepth={2}>
+					<div style={styles.title}>
+						<span style={styles.iconContainer}><BackendIcon style={styles.icon} /></span>
+						<p style={styles.headingStyle}>Back-end Development</p>
 					</div>
+					<div style={styles.subtitleContainer}>I work with <span style={styles.keyword}>Node.js</span>. My proficiency with back-end development includes:</div>
+					<ul>
+						<li>Developing backend services using RESTful architecture</li>
+						<li>Integration of third party API services (Facebook, Twitter, and Instagram)</li>
+						<li>Basic knowledge of cloud storage services (Amazon S3) and databases (MySQL)</li>
+						<li>Performing integration testing with Mocha, Sinon and Chai</li>
+					</ul>
+				</Paper>
+			</div>
+		)
+	}
+
+	renderFrontendContainer () {
+
+		return (
+			<div style={cardContainer}>
+				<Paper style={paperStyle} zDepth={2}>
+					<div style={styles.title}>
+						<span style={styles.iconContainer}><FrontendIcon style={styles.icon} /></span>
+						<p style={styles.headingStyle}>Front-end Development</p>
+					</div>
+					<div style={styles.subtitleContainer}>I work with <span style={styles.keyword}>React</span>. My expertise with front-end development includes:</div>
+					<ul>
+						<li>Creating responsive web applications using layout frameworks (Flexbox, Radium)</li>
+						<li>Maintaining state management with Redux and Reselect</li>
+						<li>Developing React Native applications</li>
+						<li>Increasing code reusability with higher-order components</li>
+					</ul>
+				</Paper>
+			</div>
+		)
+	}
+
+	render () {
+
+		return (
+			<div style={styles.container}>
+				<div style={sectionWidthContainer}>
+					{this.renderFrontendContainer()}
+					{this.renderBackendContainer()}
 				</div>
 			</div>
 		)
