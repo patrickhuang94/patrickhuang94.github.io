ba36025d15f1bc0f4352a2ca886ce356aa4ce0e0
Patrick Huang <patrickhuang94@gmail.com>
2019-02-08 15:41:20 +0700

    Upgrade react-tap-event-plugin version

--- src/index.js
+++ src/index.js
@@ -9,9 +9,6 @@ import './index.css'
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
-import injectTapEventPlugin from 'react-tap-event-plugin'
-injectTapEventPlugin()
-
 const app = (
 	<MuiThemeProvider>
 		<StyleRoot>
